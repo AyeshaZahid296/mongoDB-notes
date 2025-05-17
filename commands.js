1018298d58087bdc458532e025a7d55beffd7083
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-05-17 23:39:19 +0500

    commands of mongodb

---
+++ commands.js
@@ -0,0 +1,14 @@
+show dbs //View all databases
+
+use dbName //Create a new or switch databases
+
+db //View current Database
+
+db.dropDatabase() //Delete Database
+
+show collections  //Show Collections
+
+db.createCollection('inventary') //Create a collection named 'inventary'
+
+db.inventary.drop() //Drop a collection named 'comments'
+
